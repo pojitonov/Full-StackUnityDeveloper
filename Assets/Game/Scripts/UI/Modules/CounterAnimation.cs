7a00ec5dc1a71c3806d368c7a44a6d1dff3fe2bc
pojitonov <pojitonov@gmail.com>
2024-12-11 10:07:46 +0200

    fixed counter animation to handle smaller amounts

--- Assets/Game/Scripts/UI/Modules/CounterAnimation.cs
+++ Assets/Game/Scripts/UI/Modules/CounterAnimation.cs
@@ -8,20 +8,21 @@ namespace Modules.UI
     {
         [SerializeField]
         private float _duration = 1f;
-        
+
         [SerializeField]
         private int _fps = 30;
 
         private TMP_Text _text;
         private int _currentValue;
         private Coroutine _countingCoroutine;
-        
+
         public void Initialize(TMP_Text text, int startValue)
         {
             _text = text;
             _currentValue = startValue;
             _text.text = _currentValue.ToString();
         }
+
         public void UpdateText(int newValue)
         {
             if (_countingCoroutine != null)
@@ -36,7 +37,9 @@ namespace Modules.UI
         {
             WaitForSeconds wait = new WaitForSeconds(_duration / _fps);
             int startValue = _currentValue;
-            int stepAmount = Mathf.CeilToInt((targetValue - startValue) / (_fps * _duration));
+            int totalFrames = Mathf.Max(1, Mathf.CeilToInt(_fps * _duration));
+            int stepAmount = Mathf.Max(1, Mathf.Abs(targetValue - startValue) / totalFrames) *
+                             (int)Mathf.Sign(targetValue - startValue);
 
             while (_currentValue != targetValue)
             {
