6a29d9177eceab0693479a61515fa2b1f1db5bf1
pojitonov <pojitonov@gmail.com>
2024-12-14 17:25:39 +0200

    Added PopupPresenterFactory

---
+++ Assets/Game/Scripts/UI/PlanetPopup/PlanetPopupPresenterFactory.cs
@@ -0,0 +1,10 @@
+using Game.UI.Planets;
+using Modules.Planets;
+using Zenject;
+
+namespace Game.UI.Planet
+{
+    public class PlanetPopupPresenterFactory : PlaceholderFactory<PlanetView, IPlanet, PlanetPopupPresenter>
+    {
+    }
+}
